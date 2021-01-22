9e7c9073cc3308d2e882e76b3b41db34c7577d70
BogdanVeligorskyi <bogdan.veligorskyi@gmail.com>
2021-01-22 17:34:45 +0200

    Works with negative numbers too

--- Dlg10to2.java
+++ Dlg10to2.java
@@ -15,7 +15,7 @@ import java.awt.event.ActionEvent;
 public class Dlg10to2 extends DlgType {
 
 	private final JPanel contentPanel = super.contentPanel;
-
+	
 	/**
 	 * Launch the application.
 	 */
@@ -70,11 +70,61 @@ public class Dlg10to2 extends DlgType {
 			num /= 2;
 			string_num = String.valueOf(bit) + string_num;
 		}
-		if (string_num == "")
+		if (Integer.valueOf(textField.getText()) < 0) {
+			string_num = converterMinus(num);
+			textField_1.setText(string_num);
+		}
+		else if (string_num == "")
 			textField_1.setText("0");
 		else 
 			textField_1.setText(string_num);
 		return string_num;
 	}
-
+	
+	public String converterMinus(int num) {
+		String string_num = "";
+		int abs_num = Math.abs(num);
+		while (abs_num > 0) {
+			int bit = abs_num %2;
+			abs_num /= 2;
+			string_num = String.valueOf(bit) + string_num;
+		}
+		System.out.println("2): " + string_num);
+		String[] string_bits = string_num.split("");
+		string_num = "";
+		for(String s : string_bits) {
+			System.out.println("s: " + s);
+			if (s.compareTo("1") == 0)
+				s = "0";
+			else if (s.compareTo("0") == 0)
+				s = "1";
+		string_num = string_num + s;
+		}
+		System.out.println("3) inversed: " + string_num);
+		int length = string_num.length();
+		String [] arr_string = string_num.split("");
+		Integer[] arr_num = new Integer[string_num.length()];
+		for (int i = 0; i < arr_num.length; i++) {
+			arr_num[i] = Integer.parseInt(arr_string[i]);
+		}
+		int num_10th = 0;
+		for (int j = 0; j < arr_num.length; j++) {
+			int product = (int) (arr_num[j] * Math.pow(2, arr_num.length -j -1));
+			num_10th = num_10th + product;
+		}
+		num_10th+=1;
+		string_num = "";
+		while (num_10th > 0) {
+			int bit = num_10th %2;
+			num_10th /= 2;
+			string_num = String.valueOf(bit) + string_num;
+		}
+		while (string_num.length() < length) {
+			string_num = "0" + string_num;
+		}
+		System.out.println("4): " + string_num);
+		string_num = "1" + string_num;
+		return string_num;
+	}
+	
 }

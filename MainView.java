415759c46f3e13dabc964e52359b42a5b741e3b4
BogdanVeligorskyi <bogdan.veligorskyi@gmail.com>
2021-01-28 20:39:26 +0200

    Edited vars` names

--- MainView.java
+++ MainView.java
@@ -13,7 +13,7 @@ import java.awt.event.ActionEvent;
 
 public class MainView {
 
-	private JFrame frmCalculator;
+	private JFrame frame;
 	private GreetingView greetingView = null;
 	private MainView mainView = null;
 
@@ -25,7 +25,7 @@ public class MainView {
 			public void run() {
 				try {
 					MainView window = new MainView();
-					window.frmCalculator.setVisible(true);
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,62 +44,63 @@ public class MainView {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmCalculator = new JFrame();
-		frmCalculator.setTitle("Calculator");
-		frmCalculator.getContentPane().setForeground(Color.ORANGE);
-		frmCalculator.setBounds(100, 100, 450, 300);
-		frmCalculator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmCalculator.getContentPane().setLayout(null);
+		frame = new JFrame();
+		frame.setTitle("Number converter");
+		frame.getContentPane().setForeground(Color.ORANGE);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setBackground(Color.ORANGE);
 		
-		JButton btnNewButton = new JButton("10 to 2");
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btn10to2 = new JButton("10 to 2");
+		btn10to2.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btn10to2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			Dlg10to2 dialog = new Dlg10to2();
 			dialog.setVisible(true);
 			}
 		});
-		btnNewButton.setBounds(12, 136, 97, 25);
-		frmCalculator.getContentPane().add(btnNewButton);
+		btn10to2.setBounds(12, 136, 97, 25);
+		frame.getContentPane().add(btn10to2);
 		
-		JButton btnTo = new JButton("10 to 8");
-		btnTo.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnTo.addActionListener(new ActionListener() {
+		JButton btn10to8 = new JButton("10 to 8");
+		btn10to8.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btn10to8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			Dlg10to8 dialog = new Dlg10to8();
 			dialog.setVisible(true);
 			}
 		});
-		btnTo.setBounds(110, 136, 97, 25);
-		frmCalculator.getContentPane().add(btnTo);
+		btn10to8.setBounds(110, 136, 97, 25);
+		frame.getContentPane().add(btn10to8);
 		
-		JButton btnTo_1 = new JButton("10 to 16");
-		btnTo_1.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnTo_1.addActionListener(new ActionListener() {
+		JButton btn10to16 = new JButton("10 to 16");
+		btn10to16.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btn10to16.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			Dlg10to16 dialog = new Dlg10to16();
 			dialog.setVisible(true);
 			}
 		});
-		btnTo_1.setBounds(208, 136, 97, 25);
-		frmCalculator.getContentPane().add(btnTo_1);
+		btn10to16.setBounds(208, 136, 97, 25);
+		frame.getContentPane().add(btn10to16);
 		
-		JButton btnTo_2 = new JButton("2 to 10");
-		btnTo_2.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnTo_2.addActionListener(new ActionListener() {
+		JButton btn2to10 = new JButton("2 to 10");
+		btn2to10.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btn2to10.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			Dlg2to10 dialog = new Dlg2to10();
 			dialog.setVisible(true);
 			}
 		});
-		btnTo_2.setBounds(307, 136, 97, 25);
-		frmCalculator.getContentPane().add(btnTo_2);
+		btn2to10.setBounds(307, 136, 97, 25);
+		frame.getContentPane().add(btn2to10);
 		
-		JLabel lblNewLabel = new JLabel("Choose your task");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		lblNewLabel.setBounds(12, 47, 408, 31);
-		frmCalculator.getContentPane().add(lblNewLabel);
+		JLabel lblMain = new JLabel("Choose your task");
+		lblMain.setHorizontalAlignment(SwingConstants.CENTER);
+		lblMain.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblMain.setBounds(12, 47, 408, 31);
+		frame.getContentPane().add(lblMain);
 		
 		JButton btnBack = new JButton("Back");
 		btnBack.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -112,11 +113,11 @@ public class MainView {
 			}
 		});
 		btnBack.setBounds(161, 196, 97, 25);
-		frmCalculator.getContentPane().add(btnBack);
+		frame.getContentPane().add(btnBack);
 	}
 	
 	public JFrame getFrame() {
-		return frmCalculator;
+		return frame;
 	}
 
 }

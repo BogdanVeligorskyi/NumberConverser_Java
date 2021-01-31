d7d1646fc20b82dc47f22a886c56988bcd46ced3
BogdanVeligorskyi <bogdan.veligorskyi@gmail.com>
2021-01-31 15:46:49 +0200

    Cleaned code

--- DlgType.java
+++ DlgType.java
@@ -1,13 +1,11 @@
 package calculator;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
+import java.awt.BorderLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
